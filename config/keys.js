8ee8a5f42a511d9f28591073866d26507763d9ff
Het Patel <hetpatel.hp4199@gmail.com>
2021-12-24 10:42:27 +0530

    update keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //mongo connection string
 module.exports = {
 	MongoURI:
-		"mongodb+srv://het123:het123@cluster0.mhlqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+	"mongodb+srv://het123:het123@cluster0.mhlqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
